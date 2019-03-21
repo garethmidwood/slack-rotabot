e14bdf1dd19ebe5ca2fe466d89e9b1c58f9c9b0a
Gareth Midwood <gareth.midwood@creode.co.uk>
2019-03-21 10:15:41 +0000

    Immediately acknowledge slash commands, always send responses as delayed

--- skills/slash.js
+++ skills/slash.js
@@ -9,12 +9,16 @@ module.exports = function(controller) {
 
 
     controller.on('slash_command',function(bot,message) {
+        // we're always going to send an immediate response. 
+        // this means that we must ALWAYS use delayed replies, no immediate ones
+        bot.replyAcknowledge();
+
         switch (message.command) {
             case '/rota':
                 rotaResponse(bot, message);
             break;
             default:
-                bot.replyPrivate(message, 'Your command cannot be handled by the rota bot');
+                bot.replyPrivateDelayed(message, 'Your command cannot be handled by the rota bot');
         }
     })
 
@@ -52,33 +56,33 @@ module.exports = function(controller) {
         var rotaName = getFirstWord(message.text);
 
         if (rotaName.length == 0) {
-            bot.replyPrivate(message, 'Please specify which rota you want to lookup, e.g. `/rota brews`');
+            bot.replyPrivateDelayed(message, 'Please specify which rota you want to lookup, e.g. `/rota brews`');
         } else if (rotas[rotaName] != null) {
-            bot.replyPrivate(message, rotas[rotaName].users[0]);
+            bot.replyPrivateDelayed(message, rotas[rotaName].users[0]);
 
             if (rotas[rotaName].users.length > 0) {
                 var pointer = rotas[rotaName].pointer;
-                bot.replyPrivate(message, rotas[rotaName].users[pointer] + ' is currently active on the *' + rotaName + '* rota');
+                bot.replyPrivateDelayed(message, rotas[rotaName].users[pointer] + ' is currently active on the *' + rotaName + '* rota');
             } else {
-                bot.replyPrivate(message, 'There are no people on the *' + rotaName + '* rota');
+                bot.replyPrivateDelayed(message, 'There are no people on the *' + rotaName + '* rota');
             }
         } else {
-                bot.replyPrivate(message, '*' + rotaName + '* rota could not be found');
+                bot.replyPrivateDelayed(message, '*' + rotaName + '* rota could not be found');
         }
     }
 
     function rotaHelp(bot, message, args) {
-        bot.replyPrivate(message, 'Need some help eh buddy?!'); 
+        bot.replyPrivateDelayed(message, 'Need some help eh buddy?!'); 
     }
 
     function rotaCreate(bot, message, args) {
         var rotaName = getFirstWord(args);
 
         if (rotaName.length == 0) {
-            bot.replyPrivate(message, 'You must provide a rota name to create, e.g. `/rota create brews`');
+            bot.replyPrivateDelayed(message, 'You must provide a rota name to create, e.g. `/rota create brews`');
         } else {
             rotas[rotaName] = {'pointer': 0, 'users' : {}};
-            bot.replyPublic(message, 'Created empty rota *' + rotaName + '*');
+            bot.replyPublicDelayed(message, 'Created empty rota *' + rotaName + '*');
         }
     }
 
@@ -86,12 +90,12 @@ module.exports = function(controller) {
         var rotaName = getFirstWord(args);
 
         if (rotaName.length == 0) {
-            bot.replyPrivate(message, 'You must provide a rota name to delete, e.g. `/rota delete brews`');
+            bot.replyPrivateDelayed(message, 'You must provide a rota name to delete, e.g. `/rota delete brews`');
         } else if (rotas[rotaName] != null) {
             delete rotas[rotaName];
-            bot.replyPrivate(message, '*' + rotaName + '* has been deleted');
+            bot.replyPrivateDelayed(message, '*' + rotaName + '* has been deleted');
         } else {
-            bot.replyPrivate(message, '*' + rotaName + '* rota could not be found');
+            bot.replyPrivateDelayed(message, '*' + rotaName + '* rota could not be found');
         }
     }
 
@@ -99,20 +103,20 @@ module.exports = function(controller) {
         var rotaName = getFirstWord(args);
 
             if (rotaName.length == 0) {
-                bot.replyPrivate(message, 'You must specify a rota and who you want to add to it. e.g. `/rota add brews @dave`');
+                bot.replyPrivateDelayed(message, 'You must specify a rota and who you want to add to it. e.g. `/rota add brews @dave`');
             } else if (rotas[rotaName] != null) {
                 // get the user details
                 var args = args.replace(rotaName, '').trim();       
                 var user = getFirstWord(args);
 
                 if (args.length == 0) {
-                    bot.replyPrivate(message, 'You must specify a user to add to the rota, e.g. `/rota add brews @dave`');
+                    bot.replyPrivateDelayed(message, 'You must specify a user to add to the rota, e.g. `/rota add brews @dave`');
                 } else {
                     rotas[rotaName]['users'][user] = user;
-                    bot.replyPrivate(message, 'Adding *' + user + '* to *' + rotaName + '*');
+                    bot.replyPrivateDelayed(message, 'Adding *' + user + '* to *' + rotaName + '*');
                 }
             } else {
-                bot.replyPrivate(message, '*' + rotaName + '* rota was not found. Create it with `/rota create ' + rotaName + '`');
+                bot.replyPrivateDelayed(message, '*' + rotaName + '* rota was not found. Create it with `/rota create ' + rotaName + '`');
             }
     }
 
@@ -120,22 +124,22 @@ module.exports = function(controller) {
         var rotaName = getFirstWord(args);
 
         if (rotaName.length == 0) {
-            bot.replyPrivate(message, 'You must provide a rota name to remove people from, e.g. `/rota remove brews @dave`');
+            bot.replyPrivateDelayed(message, 'You must provide a rota name to remove people from, e.g. `/rota remove brews @dave`');
         } else if (rotas[rotaName] != null) {
             // get the user details
             var args = args.replace(rotaName, '').trim();
             var user = getFirstWord(args);
                 
             if (args.length == 0) {
-                bot.replyPrivate(message, 'You must specify a user to remove from the rota, e.g. `/rota remove brews @dave`');
+                bot.replyPrivateDelayed(message, 'You must specify a user to remove from the rota, e.g. `/rota remove brews @dave`');
             } else if (rotas[rotaName]['users'][user] != null) {
                 delete rotas[rotaName]['users'][user];
-                bot.replyPrivate(message, 'Removed *' + user + '* from *' + rotaName + '*');
+                bot.replyPrivateDelayed(message, 'Removed *' + user + '* from *' + rotaName + '*');
             } else {
-                bot.replyPrivate(message, '*' + user + '* is not on the *' + rotaName + '* rota');
+                bot.replyPrivateDelayed(message, '*' + user + '* is not on the *' + rotaName + '* rota');
             }
         } else {
-            bot.replyPrivate(message, '*' + rotaName + '* rota could not be found');
+            bot.replyPrivateDelayed(message, '*' + rotaName + '* rota could not be found');
         }
     }
 
@@ -143,18 +147,17 @@ module.exports = function(controller) {
         var rotaName = getFirstWord(args);
 
         if (Object.keys(rotas).length == 0) {
-            bot.replyAcknowledge;
             bot.replyPublicDelayed(message, 'There are no existing rotas');
         } else if (rotaName.length == 0) {
-            bot.replyPrivate(message, 'Existing rotas: ' + Object.keys(rotas).join(', '));
+            bot.replyPrivateDelayed(message, 'Existing rotas: ' + Object.keys(rotas).join(', '));
         } else if (rotas[rotaName] != null) {
             if (Object.keys(rotas[rotaName]['users']).length == 0) {
-                bot.replyPrivate(message, '*' + rotaName + '* is empty. Add members by running `/rota add ' + rotaName + ' @dave`');
+                bot.replyPrivateDelayed(message, '*' + rotaName + '* is empty. Add members by running `/rota add ' + rotaName + ' @dave`');
             } else {
-                bot.replyPrivate(message, '*' + rotaName + '* rota has the following members: ' + Object.keys(rotas[rotaName]['users']).join(','));
+                bot.replyPrivateDelayed(message, '*' + rotaName + '* rota has the following members: ' + Object.keys(rotas[rotaName]['users']).join(','));
             }
         } else {
-            bot.replyPrivate(message, '*' + rotaName + '* rota was not found. Create it with `/rota create ' + rotaName + '`');
+            bot.replyPrivateDelayed(message, '*' + rotaName + '* rota was not found. Create it with `/rota create ' + rotaName + '`');
         }
     }
 }
